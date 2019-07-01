3ef5f645c7d1bdd5b6fd222ee26a75a6cf181aee
Albert <albert5162@mail.ru>
2019-07-02 00:47:45 +0300

    add solution for 4-module 2-task

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -1,19 +1,56 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
-
+const fs = require('fs')
+const LimitSizeStream = require('./LimitSizeStream');
 const server = new http.Server();
 
 server.on('request', (req, res) => {
-  const pathname = url.parse(req.url).pathname.slice(1);
+        const limitedStream = new LimitSizeStream({ limit: 1048576}); // 8 байт
+        const pathname = url.parse(req.url).pathname.slice(1);
+        if (pathname.split('/').length > 1) {
+            res.statusCode = 400;
+            res.end('Not implemented');
+            return
+        }
+        const filepath = path.join(__dirname, 'files', pathname);
+        const filepath1 = path.join(__dirname, 'files');
 
-  const filepath = path.join(__dirname, 'files', pathname);
+        if (!fs.existsSync(filepath1)) {
+            fs.mkdir(filepath1, () => {
+            })
+        }
+        if (fs.existsSync(filepath)) {
+            res.statusCode = 409;
+            res.end('Not implemented');
+            return
+        }
+        const stream  = fs.createWriteStream(filepath)
+        limitedStream.pipe(stream)
+        req.pipe(limitedStream)
 
+        limitedStream.on('error', () => {
+            res.statusCode = 413;
+            res.end('Not implemented');
+            fs.unlink(filepath, () => {
+
+            })
+        })
+        req.on('data', function(chunk) {
+            limitedStream.write(chunk)
+        });
+        req.on('end', () => {
+            res.statusCode = 201;
+            res.end('a')
+        })
+        server.on('clientError', (err, socket) => {
+            fs.unlink(filepath, () => {
+
+            })
+        })
   switch (req.method) {
     case 'POST':
-
       break;
-
     default:
       res.statusCode = 501;
       res.end('Not implemented');
