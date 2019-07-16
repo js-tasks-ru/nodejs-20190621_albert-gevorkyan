eb7fe6250b71ab9d60a7c6072bb984704ab50196
Albert <albert5162@mail.ru>
2019-07-17 00:16:48 +0300

    add solution for 6-module 3-task 7-module 1-task

--- 6-module/3-task/models/Product.js
+++ 6-module/3-task/models/Product.js
@@ -1,35 +1,39 @@
 const mongoose = require('mongoose');
 const connection = require('../libs/connection');
-
 const productSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true,
   },
-  
+
   description: {
     type: String,
     required: true,
   },
-  
+
   price: {
     type: Number,
     required: true,
   },
-  
+
   category: {
     type: mongoose.Types.ObjectId,
     ref: 'Category',
     required: true,
   },
-  
+
   subcategory: {
     type: mongoose.Types.ObjectId,
     required: true,
   },
-  
+
   images: [String],
-  
+
 });
+productSchema.index({title: "text", description: "text"}, { default_language: "russian",  name: "TextSearchIndex",
+weights: {
+       title: 10,
+       description: 5
+     }})
 
 module.exports = connection.model('Product', productSchema);
