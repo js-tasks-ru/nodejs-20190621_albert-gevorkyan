6780001c92cbe1b776bb2ccee35ab2a14c62724b
Albert <albert5162@mail.ru>
2019-08-04 14:18:48 +0300

    add solution for 8-module 2-task

--- 8-module/2-task/controllers/orders.js
+++ 8-module/2-task/controllers/orders.js
@@ -1,7 +1,25 @@
 const Order = require('../models/Order');
 
 module.exports.checkout = async function checkout(ctx, next) {
+    try {
+        var asd = await Order.create({
+            user: ctx.user,
+            product: ctx.request.body.product,
+            phone: ctx.request.body.phone,
+            address: ctx.request.body.address
+        })
+        ctx.body = {order : asd._id};
+    } catch (e) {
+        var f = {errors : {}}
+        for (var i in e.errors) {
+            f.errors[i] = e.errors[i].message
+        }
+        ctx.status = 400
+        ctx.body = f
+    }
 };
 
 module.exports.getOrdersList = async function ordersList(ctx, next) {
+    var ords = await Order.find({ user: ctx.user._id }).populate('product')
+    ctx.body = {orders: ords}
 };
